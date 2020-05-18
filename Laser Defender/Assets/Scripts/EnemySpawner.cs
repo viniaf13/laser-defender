4f9eefb7ea66ed6fc74ffebe58557adff86fa9a4
viniaf13 <viniaf13@gmail.com>
2020-05-18 17:14:14 -0300

    created damage dealer class

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -6,11 +6,16 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs = default;
     [SerializeField] int startingWave = 0;
+    [SerializeField] bool loop = false;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        StartCoroutine(SpawnAllWaves());
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (loop);
+
     }
 
     private IEnumerator SpawnAllWaves()
