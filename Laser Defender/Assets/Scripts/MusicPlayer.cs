7caa3eb2d5281745c442fbcca05fe39f100a29c7
viniaf13 <viniaf13@gmail.com>
2020-05-25 13:41:55 -0300

    added music player to all scenes

---
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    //Just training setting up a singleton
+    /*void Awake()
+    {
+        SetupSingleton();
+    }
+
+    private void SetupSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }*/
+}
